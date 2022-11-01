af0ee7bef487fc74f0defb82e86533374e03f242
Jonathan <jonathhl@stud.ntnu.no>
2022-11-01 10:52:25 +0100

    Finished draft of models.
    Do not know if works

--- models.py
+++ models.py
@@ -1,21 +1,26 @@
-import gym
-import random
-import numpy as np
-import torch.nn as nn
 import torch.nn.functional as F
-from collections import deque
+import torch.nn as nn
 
+"""
+    Model containing 3 convolutional layers, as well as 3 layers of batch normalization.
+    There is also two fully connected layers.
 
+    The forward function also applies relu to the layers, before returning the head output of the class.
+
+    Input:
+        input:shape: consists of channel, weight and height, which are all the inputs that should be given from the environment.
+        n_actions: the number of actions given from the action_space.
+"""
 class DQN(nn.Module):
-    def __init__(self, in_channels=4, n_actions=14):
+    def __init__(self, input_shape, n_actions):
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=8, stride=4)
+        self.conv1 = nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4)
         self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
         self.bn2 = nn.BatchNorm2d(64)
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
         self.bn3 = nn.BatchNorm2d(64)
-        self.fc4 = nn.Linear(7 * 7 * 64, 512)
+        self.fc = nn.Linear(7 * 7 * 64, 512)
         self.head = nn.Linear(512, n_actions)
 
     def forward(self, x):
